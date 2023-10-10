04ee8b8a31b4d4c71a137bd7cbc93ecd160c6e81
Dmitriy Tkachenko <greatstep93@gmail.com>
2023-10-10 06:32:56 +0400

    Initial commit

---
+++ src/main/java/ru/greatstep/db_greatstep/DbGreatstepApplication.java
@@ -0,0 +1,13 @@
+package ru.greatstep.db_greatstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbGreatstepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbGreatstepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/greatstep/db_greatstep/DbGreatstepApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.greatstep.db_greatstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbGreatstepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
