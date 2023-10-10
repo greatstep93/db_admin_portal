3497ad43db1a5779f12bf770569b23b3aaaac10d
Dmitriy Tkachenko <greatstep93@gmail.com>
2023-10-10 13:14:47 +0400

    update db_admin_portal schema

---
+++ changelog/db/1.create-db_admin_portal/insert_roles.sql
@@ -0,0 +1,7 @@
+INSERT INTO db_admin_portal.roles(role_name, is_active)
+VALUES ('ADMIN', true),
+       ('USER', true),
+       ('DEVELOPER', true),
+       ('QA', true),
+       ('DESIGNER', true),
+       ('GUEST', true)
